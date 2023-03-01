3be26d9cc5d9854c96b31ef1be3cc53e862474e2
tando <tando@LAPTOP-KJNPQ5TK>
2023-03-01 16:21:21 +0100

    workss

--- testconsole01.12/Program.cs
+++ testconsole01.12/Program.cs
@@ -14,11 +14,14 @@ class Program
     {
         string filepath = @"C:\classes\masterarbeit\instances\N_T\N_T\N1c.ins2D";
         Filehandler fhandler = new Filehandler(filepath);
-        foreach(Package2D p2 in fhandler.Packagelist)
-        {
-            Console.WriteLine(p2);
-        }
+        //foreach(Package2D p2 in fhandler.Packagelist)
+        //{
+        //    Console.WriteLine(p2);
+        //}
 
+        Extreme_Algorithms algos = new Extreme_Algorithms();
+        algos.Input_packages = fhandler.Packagelist.ToList();
+        algos.Main_SU();
 
 
 
